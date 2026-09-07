acbbf8c0f2d87e46b96aa79473138156b5f9056a
antonriabokon <195663496+antonriabokon@users.noreply.github.com>
2026-09-07 13:03:37 +0300

    stack panel logic added

--- ToDoApplication/MainWindow.xaml.cs
+++ ToDoApplication/MainWindow.xaml.cs
@@ -20,5 +20,24 @@ namespace ToDoApplication
         {
             InitializeComponent();
         }
+
+        private void AddToDoButton_Click(object sender, RoutedEventArgs e)
+        {
+            string todoText = ToDoInput.Text;
+
+            if (!string.IsNullOrEmpty(todoText))
+            {
+                TextBlock todoItem = new TextBlock
+                {
+                    Text = todoText,
+                    Margin = new Thickness(10),
+                    Foreground = new SolidColorBrush(Colors.White)
+                };
+
+
+                ToDoList.Children.Add(todoItem);
+                ToDoInput.Clear();
+            }
+        }
     }
 }
